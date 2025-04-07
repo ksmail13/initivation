aa728b282aeb1606076dbe9bade81eb522b136da
ksmail13 <ksmail13@gmail.com>
2025-04-08 03:54:16 +0900

    vh -> dvh

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import DirectionsPage from './pages/DirectionsPage';
 
 function App() {
   return (
-    <div className="h-screen overflow-hidden bg-stone-200 font-nanum break-keep">
-      <div className="w-screen h-screen overflow-y-scroll snap-y snap-mandatory"> {/* 중앙 정렬 및 최대 너비 설정 */}
+    <div className="h-dvh overflow-hidden bg-stone-200 font-nanum break-keep">
+      <div className="w-screen h-dvh overflow-y-scroll snap-y snap-mandatory"> {/* 중앙 정렬 및 최대 너비 설정 */}
         <InvitationPage />
         <GalleryPage />
         <DirectionsPage />
--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 function Card({ children }: PropsWithChildren<{ no?: number }>) {
   return (
-    <div className='w-full h-screen flex items-center justify-center snap-start'>
+    <div className='w-full h-dvh flex items-center justify-center snap-start'>
       <div className="w-5/6 h-9/10 md:h-2/3 max-w-4xl rounded-lg shadow-2xl p-8 bg-stone-100 text-stone-700">
         {children}
       </div>
